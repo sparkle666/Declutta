ea4642af582a4e95524cb9e4984fc49c18a96c97
sparkle666 <sikky606@gmail.com>
2024-01-10 13:25:13 +0100

    initial project setup

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello Worls!!</Text>
+      <StatusBar style="auto" />
+      <Text text30 >Text goes here</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
